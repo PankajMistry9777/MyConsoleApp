7afbb1064e20a9c0bf56b92b064eb9f23df01837
Pankaj <pankajkumar.p@m2pfintech.com>
2024-07-08 16:27:04 +0530

    first commit - console app

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MyConsoleApp
+{
+    class Program
+    {
+       static void Main(string[] args)
+        {
+            Console.WriteLine("Sample Data in Console App Jenkins-TEST");
+
+            // Sample data
+            var names = new string[] {  "Chandan","Suraj","Pankaj", "Karan" };
+
+            foreach (var name in names)
+            {
+                Console.WriteLine($"Hello, {name}!");
+            }
+        }
+    }
+}
